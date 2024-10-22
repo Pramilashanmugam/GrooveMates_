c8b197282c77c31e883fbf39a99a89c8ae6b4b1e
Pramila Shanmugam <pramila.sl@gmail.com>
2024-10-22 16:53:57 +0000

    add profiles/urls,serializers files for deserialise data

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(APIView):
+    """
+    API endpoint that allows users to retrieve a list of profiles.
+
+    This class-based view handles GET requests to return all profile instances
+    from the database. It uses the Profile model and ProfileSerializer to
+    serialize the data and return it as a JSON response.
+
+    Methods:
+        get(request): Handles GET requests and returns a serialized list of
+        Profile objects.
+    Args:
+        request (HttpRequest): The HTTP request object that contains metadata
+        about the request.
+    Returns:
+        Response: A JSON response containing serialized data of all Profile
+        instances.
+    """
+    def get(self, request):
+        """
+        Retrieves all profiles from the database and returns them in serialized
+        form.
+        This method queries the Profile model for all instances, serializes
+        them using ProfileSerializer, and returns the serialized data in the
+        response.
+
+        Args:
+            request (HttpRequest): The HTTP request object containing request
+            data.
+        Returns:
+            Response: A JSON response with serialized data of all Profile
+            objects.
+        """
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
