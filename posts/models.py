bda6fd4a05023828c283b3138ba59f45408b80f8
Pramila Shanmugam <pramila.sl@gmail.com>
2024-10-23 14:08:30 +0000

    add new app - posts, add file serializers. post model created and serialized

---
+++ posts/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """
+    Post model represents an event created by a user ('owner').
+    Attributes:
+    - owner: A ForeignKey linking to the User model. Indicates the user who
+    created the post.
+    - created_at: A DateTimeField automatically storing the date and time the
+    post was created.
+    - updated_at: A DateTimeField automatically updating the date and time when
+    the post is edited.
+    - event: A CharField for the name of the event, with a maximum length of
+    255 characters.
+    - description: An optional TextField for the event's description. Can be
+    left blank.
+    - location: A CharField representing the event's location, with a maximum
+    length of 255 characters.
+    - date: A DateField storing the event's date, defaulting to the current
+    date if not provided.
+    - time: A TimeField storing the event's time, defaulting to the current
+    time if not provided.
+    - image: An optional ImageField for the event's image, uploaded to the
+    'images/' directory by default.
+              If no image is provided, a default image
+              ('../default_post_xxhr8e') is used.
+    Meta:
+    - The posts are ordered in descending order of their creation time
+    (`-created_at`).
+    Methods:
+    - __str__: Returns a string representation of the post, displaying its
+    ID and event name.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    event = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=255)
+    date = models.DateField(default=timezone.now, blank=True)
+    time = models.TimeField(default=timezone.now, blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_xxhr8e', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.event_name}'
---
+++ posts/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from posts.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Post model.
+    This serializer is responsible for converting Post instances into JSON
+    format and vice versa,
+    making it suitable for API views. It also includes additional fields for
+    user-related data.
+    Fields:
+    - owner: Read-only field that displays the username of the user who owns
+    the post.
+    - is_owner: A boolean field that indicates whether the currently
+    authenticated user is the owner of the post.
+    - profile_id: Read-only field that shows the ID of the owner's profile.
+    - profile_image: Read-only field that shows the URL of the owner's profile
+    image.
+    - id: The primary key of the Post.
+    - created_at: The timestamp of when the post was created.
+    - updated_at: The timestamp of the last update made to the post.
+    - event: The event name (CharField) related to the post.
+    - description: The description of the post, which can be left blank.
+    - image: The image associated with the post, with a default set if none is
+    provided.
+    - location: The location where the event takes place.
+    - date: The date of the event.
+    - time: The time of the event.
+    Meta:
+    - The model associated with this serializer is the Post model.
+    - The `fields` attribute specifies the fields to be serialized and
+    deserialized.
+    """
+
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'event',
+            'description', 'image', 'location', 'date', 'time', 'is_owner',
+            'profile_id', 'profile_image'
+        ]
