6c9d5e3c2fd2beda700c4fd1d562518c2b643d83
Pramila Shanmugam <pramila.sl@gmail.com>
2024-10-28 21:33:52 +0000

    time and date validation updated to accept current and future upto 10years

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from posts.models import Post
-
+from datetime import datetime, timedelta
 
 class PostSerializer(serializers.ModelSerializer):
     """
@@ -38,22 +38,37 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     date = serializers.DateField()
+    time = serializers.TimeField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
-            raise serializers.ValidationError(
-                'Image size is larger than 2MB'
-            )
+            raise serializers.ValidationError('Image size is larger than 2MB')
         if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width is larger than 4096px'
-            )
+            raise serializers.ValidationError('Image width is larger than 4096px')
         if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height is larger than 4096px'
-            )
+            raise serializers.ValidationError('Image height is larger than 4096px')
         return value
 
+    def validate_date(self, value):
+        today = datetime.now().date()
+        ten_years_from_now = today + timedelta(days=3650)
+
+        if value < today:
+            raise serializers.ValidationError("The date cannot be in the past.")
+        if value > ten_years_from_now:
+            raise serializers.ValidationError("The date cannot be more than 10 years in the future.")
+
+        return value
+
+    def validate_time(self, value):
+        today = datetime.now().date()
+        current_time = datetime.now().time()
+
+        # Check if the date is today and the time is in the past
+        if self.initial_data.get('date') == str(today) and value < current_time:
+            raise serializers.ValidationError("The time cannot be in the past for today's date.")
+
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -64,5 +79,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'event',
             'description', 'image', 'location', 'date', 'time', 'is_owner',
-            'profile_id', 'profile_image','image_filter'
+            'profile_id', 'profile_image', 'image_filter'
         ]
