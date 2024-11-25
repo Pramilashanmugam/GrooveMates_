8a4bd80f31f407c3ffc9d9513d8cffe51b4ba646
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-25 22:21:55 +0000

    permissions updated for reports

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -6,3 +6,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         return obj.owner == request.user
 
+class IsAdminOrCreateOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.is_authenticated
+        return request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff
--- reports/views.py
+++ reports/views.py
@@ -1,13 +1,14 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Report
 from .serializers import ReportSerializer
+from drf_api.permissions import IsAdminOrCreateOnly
 
 class ReportCreateView(generics.CreateAPIView):
     """
     View for creating a new report.
     """
     serializer_class = ReportSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAdminOrCreateOnly]
 
     def perform_create(self, serializer):
         serializer.save(reporter=self.request.user)
@@ -17,7 +18,7 @@ class ReportListView(generics.ListAPIView):
     View for admins to list all reports.
     """
     serializer_class = ReportSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrCreateOnly]
 
     def get_queryset(self):
         return Report.objects.all()
