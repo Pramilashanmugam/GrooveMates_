58502c00b7c8f3e0617d52bdf5f614d38669575d
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-26 13:56:07 +0000

    migration done on all the models and updated reports/urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -3,5 +3,5 @@ from reports import views
 
 urlpatterns = [
     path('reports/', views.ReportCreateView.as_view(), name='report-create'),
-    path('reports/admin/', views.ReportListView.as_view(), name='report-list'),
+    path('reports/<int:pk>/', views.ReportListView.as_view(), name='report-list'),
 ]
