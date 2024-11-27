41813d21c472401af472d58667fdce02e707ef3d
Pramila Shanmugam <pramila.sl@gmail.com>
2024-11-27 22:21:14 +0000

    shared-posts/ new url included

--- shares/urls.py
+++ shares/urls.py
@@ -4,4 +4,5 @@ from shares import views
 urlpatterns = [
     path('shares/', views.ShareList.as_view(), name='share-list'),
     path('shares/<int:pk>/', views.ShareDetail.as_view(), name='share-detail'),
+    path('shared-posts/', views.UserSharedPostsView.as_view(), name='user-shared-posts'),
 ]
