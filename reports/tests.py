d1d0625744989cf822611bc2e9da4d6094cea7d2
Pramila Shanmugam <pramila.sl@gmail.com>
2024-12-04 08:37:16 +0000

    deleted a testcase in reports/tests.py

--- reports/tests.py
+++ reports/tests.py
@@ -124,23 +124,6 @@ class ReportTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['reason'], "spam")
 
-    def test_update_report_status_admin_user(self):
-        """
-        Test that an admin user can update the status of a report.
-
-        - Creates a report.
-        - Logs in as an admin and updates the status of the report.
-        - Asserts that the report's status is updated correctly.
-        """
-        report = Report.objects.create(
-            reporter=self.user2, post=self.post, reason="spam"
-            )
-        self.client.login(username="admin", password="adminpass")
-        data = {"status": "Reviewed"}
-        response = self.client.patch(self.report_detail_url(report.id), data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Report.objects.first().status, "Reviewed")
-
     def test_update_report_status_non_admin_user(self):
         """
         Test that a non-admin user cannot update the status of a report.
